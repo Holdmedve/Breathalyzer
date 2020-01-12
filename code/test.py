d4e18773ec216c0003c4bff8bce4851e0c96807e
Holdmedve <talos.boti@gmail.com>
2020-01-12 23:33:04 +0100

    typo

--- code/test.py
+++ code/test.py
@@ -5,7 +5,7 @@ import Adafruit_MCP3008
 # Hardware SPI configuration
 SPI_PORT    = 0
 SPI_DEVICE  = 0
-mcp = Adafruit_MCP3008.MCP_3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 print('soil.py is running...')
 while True:
