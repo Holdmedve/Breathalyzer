9def9e1e2143e1cd0d5db3285252ccf0fff74ad1
Holdmedve <talos.boti@gmail.com>
2020-01-12 23:20:48 +0100

    test code for the sensor

---
+++ code/test.py
@@ -0,0 +1,13 @@
+import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Hardware SPI configuration
+SPI_PORT    = 0
+SPI_DEVICE  = 0
+mcp = Adafruit_MCP3008.MCP_3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+print('soil.py is running...')
+while True:
+    print(mcp.read_adc(0))
+    time.sleep(1)
