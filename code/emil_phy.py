2f100b2d8f3ec3d795f9efbd63e37ffb17a654ec
masik medve <rijonatan@gmail.com>
2020-01-13 17:26:23 +0100

    email with html

--- code/emil_phy.py
+++ code/emil_phy.py
@@ -2,38 +2,28 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-
-sender_email = "@gmail.com"
-receiver_email = "@indamail.hu"
-password = "ide jön a jelszó"
+sender_email = "radvanszkyij@gmail.com"
+receiver_email = "radvanszkyij@gmail.com"
+password = "Bocs10eves"
 
 message = MIMEMultipart("alternative")
-message["Subject"] = "targy"
+message["Subject"] = "üzenet a földnedvesség-mérőtől"
 message["From"] = sender_email
 message["To"] = receiver_email
 
-# Create the plain-text and HTML version of your message
-text = """\
-Hi,
-html: """
+
 html = """\
 <html>
-<p align="center">
-közép
-</p>
-
-
+<h1 align="center">Kedves felhasználó!</h1>
+<font size="4">Az Ön által gondozott növény földjének nedvesség<br>
+tartalma a kritikus érték alá sülyedt.<br>
+Öntözze meg a növényét!</font>
+<img src="C:/Users/rijon/28767881_2038175236467148_221478428_o.png">
 </html>
 """
 
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
-
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-message.attach(part1)
-message.attach(part2)
+htmlPart = MIMEText(html, "html")
+message.attach(htmlPart)
 
 # Create secure connection with server and send email
 context = ssl.create_default_context()
@@ -42,3 +32,5 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.sendmail(
         sender_email, receiver_email, message.as_string()
     )
+
+    print('Hello ! I am finished')
