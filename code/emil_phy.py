33507d1e0ae2356a14fb8146e2172f471de08916
masik medve <rijonatan@gmail.com>
2020-01-13 18:13:26 +0100

    email with picture: some delete

--- code/emil_phy.py
+++ code/emil_phy.py
@@ -18,12 +18,6 @@ message["From"] = sender_email
 message["To"] = receiver_email
 
 
-# Create the plain-text and HTML version of your message
-#text = """\
-#Hi,
-#How are you?
-#Real Python has many great tutorials:
-#www.realpython.com"""
 html = """\
 <html>
 <h1 align="center">Kedves felhasználó!</h1>
@@ -34,15 +28,11 @@ tartalma a kritikus érték alá sülyedt.<br>
 </html>
 """
 
-# Turn these into plain/html MIMEText objects
-#part1 = MIMEText(text, "plain")
+
 part2 = MIMEText(html, "html")
 image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
 
 message.attach(image)
-#message.attach(part1)
 message.attach(part2)
 
 # Create secure connection with server and send email
